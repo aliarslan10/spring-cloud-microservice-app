e5af4d23736a389f226d432c628050a004a9ff3a
Ali Arslan <ali@blogkafem.net>
2022-02-07 21:00:37 +0300

    Add @Data, @Builder Annotations and Add Private Access Modifier to Fields of OrderCreate.java

--- order-service/src/main/java/com/example/aliarslan/orderservice/domain/order/model/OrderCreate.java
+++ order-service/src/main/java/com/example/aliarslan/orderservice/domain/order/model/OrderCreate.java
@@ -1,7 +1,12 @@
 package com.example.aliarslan.orderservice.domain.order.model;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.math.BigDecimal;
 
+@Data
+@Builder
 public class OrderCreate {
 
     OrderStatus status;
