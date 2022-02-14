062915203f7f2f32777bfcbf6a0642f6e137f176
Ali Arslan <ali@blogkafem.net>
2022-02-14 08:57:13 +0300

    Refactor and Add @NotNull Validation To OrderRequest Model

--- order-service/src/main/java/com/example/aliarslan/orderservice/infra/adapters/rest/dto/OrderRequest.java
+++ order-service/src/main/java/com/example/aliarslan/orderservice/infra/adapters/rest/dto/OrderRequest.java
@@ -1,12 +1,23 @@
 package com.example.aliarslan.orderservice.infra.adapters.rest.dto;
 
 import com.example.aliarslan.orderservice.domain.order.model.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderRequest {
 
-    OrderStatus status;
+    @NotNull
+    private Integer status;
 
-    BigDecimal price;
+    @NotNull
+    private BigDecimal price;
 }
