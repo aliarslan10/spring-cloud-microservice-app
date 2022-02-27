6b847f0b0e40bdb8367b9058599c54850d137be6
Ali Arslan <ali@blogkafem.net>
2022-02-27 23:40:17 +0300

    Add OrderResponse Builder To Order Model

--- order-service/src/main/java/com/example/aliarslan/orderservice/domain/order/model/Order.java
+++ order-service/src/main/java/com/example/aliarslan/orderservice/domain/order/model/Order.java
@@ -1,5 +1,6 @@
 package com.example.aliarslan.orderservice.domain.order.model;
 
+import com.example.aliarslan.orderservice.infra.adapters.rest.dto.OrderResponse;
 import lombok.Builder;
 import lombok.Data;
 
@@ -13,5 +14,16 @@ public class Order {
     private String id;
     private OrderStatus status;
     private BigDecimal price;
+    private String userId;
     private LocalDateTime createdDate;
+
+    public OrderResponse toResponse() {
+        return OrderResponse.builder()
+                .orderId(id)
+                .status(status)
+                .price(price)
+                .userId(userId)
+                .createdDate(createdDate)
+                .build();
+    }
 }
