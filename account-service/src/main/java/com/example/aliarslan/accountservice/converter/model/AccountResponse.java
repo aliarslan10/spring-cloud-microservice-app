7d69b2f66132d23d2aae4538ecc186a16f1807d8
Ali Arslan <ali@blogkafem.net>
2022-03-08 22:04:36 +0300

    Add AccountResponse

---
+++ account-service/src/main/java/com/example/aliarslan/accountservice/converter/model/AccountResponse.java
@@ -0,0 +1,16 @@
+package com.example.aliarslan.accountservice.converter.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class AccountResponse {
+
+    private String userId;
+    private String username;
+    private LocalDate createdAt;
+    private Boolean isActive;
+}
