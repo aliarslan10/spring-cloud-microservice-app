b2b23bdbd2f8993b72fefb89ab89556388295866
Ali Arslan <ali@blogkafem.net>
2022-03-18 23:28:12 +0300

    Import ClientService And Enable FeignClient In OrderService

--- order-service/src/main/java/com/example/aliarslan/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/aliarslan/orderservice/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.aliarslan.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.example.aliarslan.client")
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
