182c394bfab5830dc3a45cd1cddeef02fddfe5fe
Ali Arslan <ali@blogkafem.net>
2022-04-10 07:48:58 +0300

    Refactor FeignClient Communication Between Account & Order Services
    By the this refactor, FeignClient communication is done over DiscoveryServer thus requests will be load-balanced by SpringCloudRibbon

--- client-service/src/main/java/com/example/aliarslan/client/account/AccountClientService.java
+++ client-service/src/main/java/com/example/aliarslan/client/account/AccountClientService.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "account-service-client", url = "localhost:8090")
+@FeignClient(name = "ACCOUNT-SERVICE") // application name in the discovery-server
 public interface AccountClientService {
 
     @GetMapping("/accounts/{id}")
