72c3caba675d9734c1a49a69bfd25d2b2830d67b
Ali Arslan <ali@blogkafem.net>
2022-05-06 11:19:07 +0300

    Fix Null Field Problem AccountServiceResponse Of ClientService
    Related commit : https://github.com/aliarslan10/spring-cloud-microservice-app/commit/7ab8895e5bb5c6496ef37383b8c4dbaa9827357c
    
    Also, after parent module changed for client-service, the service could not utilized from lombok dependency. Instead of adding lombok dependency to client-service again, I used directly getter methods. There is no relation between null problem and lombok dependency. Not using lombok is only optional choice.

--- client-service/src/main/java/com/example/aliarslan/client/account/model/AccountServiceResponse.java
+++ client-service/src/main/java/com/example/aliarslan/client/account/model/AccountServiceResponse.java
@@ -1,11 +1,28 @@
 package com.example.aliarslan.client.account.model;
 
-import lombok.Getter;
-
-@Getter
+/**
+ * It should have same fields with same naming with AccountResponse (in AccountController)
+ * for correct mapping to prevent null fields.
+ */
 public class AccountServiceResponse {
-    private String id;
+    private String userId;
     private String username;
     private String mailAddress;
     private String isActive;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public String getIsActive() {
+        return isActive;
+    }
 }
